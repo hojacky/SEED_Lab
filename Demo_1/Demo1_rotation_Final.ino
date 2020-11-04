b8c5f7774629c6aefdadc816974bf9404cc501a4
mchidalgo <72232681+mchidalgo@users.noreply.github.com>
2020-11-04 13:47:56 -0700

    Update Demo1_rotation_Final.ino

--- Demo_1/Demo1_rotation_Final.ino
+++ Demo_1/Demo1_rotation_Final.ino
@@ -13,8 +13,29 @@
   double angularDistance = 255;//135 deg
 //  double angularDistance = 353;// 180 deg
   const double WheelRadius = 0.245;// radius of wheel(meters)
+  const double axleRadius = 0.323;//need to measure(meters)
+  const double d = .1;
+  const double N = 6.28;
+  const double halfPi = 1.35;
+  const double Pi = 3.14;
+  const double threeHalfPi = 4.67;
+  double dot_theta1 = 0;
+  double vel1 = 0;
+  double mvolt = 0;
+  volatile int aVal = 0;
+  volatile int bVal = 0;
+  volatile double time1;
+  volatile double totaltime1 = 0;
+  volatile double oldtime1 = 0;
+  volatile double t1 = 0;
   double D_L = 0;
   double D_R= 0;
+  double D =0;
+  double xnew = 0;
+  double xold = 0;
+  double ynew = 0;
+  double yold = 0;
+  double step_theta = 0;
   double leftControllerOutput;
   double rightControllerOutput;
   double leftAngularOutput;
@@ -25,14 +46,19 @@
   double Ki = 0.0001; //0.0001; //V/rad*mil
   double I_L = 0;
   double I_R;
+  double I = 0;
   double e_Lpast;
   double e_Rpast;
+  double e_past;
   int Ts = 50;
   double Tc = 0;
+  int poop = 0;
+  int lastPos = 100;
+  int quadrant = 1;
   double lefterror = 0;
   double righterror = 0;
-  double str_Err_L = 0;
-  double str_Err_R = 0;
+  double leftang = 0;
+  double rightang = 0;
   double r_L = 0; //quadrant;
   double r_R = 0; //quadrant;
   double y_L = 0;
@@ -125,6 +151,8 @@ void LeftAngle(){
  }
 
 
+
+
  void RightAngle(){
   r_R = y_R;
   y_R = motorEncoder2.read();
@@ -133,7 +161,114 @@ void LeftAngle(){
 
 
 
+
+
+ void LeftWheel(){
+ Tc = millis();
+ double r = distance; //quadrant;
+ //Serial.println("Desired Distance left");
+ //Serial.println(r);
+ double y = motorEncoder.read();
+ //Serial.println("Current Position left");
+ //Serial.println(y);
+ y = (y/1600)*PI;
+ //Serial.println(y);
+ lefterror = r + y;
+ if(Tc > 0){
+  D_L = (lefterror - e_Lpast)/Ts; //rad/mil
+  e_Lpast = lefterror;
+ }
+ else{
+  D_L = 0;
+ }
+ //Serial.println("Error Left");
+ //Serial.println(lefterror);
+ if (lefterror < 0){
+  leftControllerOutput = 0;
+ }else{
+    I_L = I_L + (Ts*lefterror); //rad*mil
+   leftControllerOutput = (Kp*lefterror);// + (Ki*I_L) + (Kd*D_L)));
+ }
+ if(leftControllerOutput > 0){
+  digitalWrite(7, LOW);
+ }else{
+  digitalWrite(7, HIGH);
+ }
+ leftControllerOutput = abs(leftControllerOutput);
+ leftControllerOutput = (leftControllerOutput/8.5)*255;
+ if(leftControllerOutput > 255){
+  leftControllerOutput = 255;
+ }
+ analogWrite(9,leftControllerOutput);
+// Serial.println("PWM voltage and Error left");
+// Serial.println(leftControllerOutput);
+ Ts = Ts - Tc;
+ delay(50);
+
+ 
+  //if a character is detected on the serial monitor, reset wheel to zero
+//   if (Serial.available()) {
+//    Serial.read();
+//    Serial.println("Reset both knobs to zero");
+//    motorEncoder.write(0);
+//  }
+ }
+//
+//
+//
+//
+ void RightWheel(){
+ Tc = millis();
+ double r = distance; //quadrant;
+ double y = motorEncoder2.read();
+ y = (y/1600)*PI;
+ righterror = r - y;
+ if(Tc > 0){
+  D_R = (righterror - e_Rpast)/Ts; //rad/mil
+  e_Rpast = righterror;
+ } else{
+  D_R = 0;
+ }
+ if (righterror < 0){
+  rightControllerOutput = 0;
+ }else{
+   I_R = I_R + (Ts*righterror); //rad*mil
+   rightControllerOutput = (Kp*righterror);// + (Ki*I_R) + (Kd*D_R);
+ } 
+// Serial.println("Error Right");
+// Serial.println(righterror);
+
+ if(angularDistance < 0){
+  digitalWrite(8, LOW);
+ }else{
+  digitalWrite(8, HIGH);
+ }
+ rightControllerOutput = abs(rightControllerOutput);
+ rightControllerOutput = (rightControllerOutput/8.5)*255;
+ if(rightControllerOutput > 255){
+  rightControllerOutput = 255;
+ }
+ analogWrite(10,rightControllerOutput);
+//  Serial.println("PWM voltage and Error right");
+// Serial.println(rightControllerOutput);
+
+ Ts = Ts - Tc;
+ delay(50);
+
+ 
+  //if a character is detected on the serial monitor, reset wheel to zero
+//   if (Serial.available()) {
+//    Serial.read();
+//    Serial.println("Reset both knobs to zero");
+//    motorEncoder2.write(0);
+//   }
+ }
+
  void loop() { 
+// if(angularDistance != 0){
+//    leftAngular(); 
+//    rightAngular();
+// }
 
 if (turn) {
 ///////////////////////////////////////////Left and Right Angle////////////////////////////////////////////////////
@@ -143,7 +278,11 @@ Tc = millis();
  y_R = (y_R/1600)*PI;
  
  lefterror = angularDistance + y_L;
- righterror = angularDistance - y_R;
+ righterror = angularDistance - y_R;//chanegd this!!!!!!!!!!!!!!!!!!!!!!!!
+// Serial.println("ERROR LEFT");
+// Serial.println(lefterror);
+// Serial.println("ERROR RIGHT");
+// Serial.println(righterror);
 
  if(Tc > 0){
   D_L = (y_L - r_L)/Ts; //rad/mil
@@ -219,75 +358,11 @@ Tc = millis();
       //distance = 12*1.0*2*PI / 5.75*PI;
     }
     straight = 1;
-    r_L = distance;
-    r_R = distance;
   }
   if (straight) {
-    Tc = millis();
-    y_L = (y_L/1600)*PI;
-    y_R = (y_R/1600)*PI;
-    strErr_L = distance + y_L;
-    strErr_R = distance - y_R;
-   
-    if(Tc > 0){
-    D_L = (lefterror - e_Lpast)/Ts; //rad/mil
-    e_Lpast = lefterror;
-   }
-   else{
-    D_L = 0;
-   }
- 
-   if(Tc > 0){
-    D_R = (righterror - e_Rpast)/Ts; //rad/mil
-    e_Rpast = righterror;
-   } else{
-    D_R = 0;
-   }
-   
-   if (lefterror < 0){
-    leftControllerOutput = 0;
-   }else{
-      I_L = I_L + (Ts*lefterror); //rad*mil
-      leftControllerOutput = (Kp*lefterror);// + (Ki*I_L) + (Kd*D_L)));
-   }
-   if (righterror < 0){
-    rightControllerOutput = 0;
-   }else{
-     I_R = I_R + (Ts*righterror); //rad*mil
-     rightControllerOutput = (Kp*righterror);// + (Ki*I_R) + (Kd*D_R);
-   } 
-   
-   if(leftControllerOutput > 0){
-    digitalWrite(7, LOW);
-   }else{
-    digitalWrite(7, HIGH);
-   }
-   if(angularDistance < 0){
-    digitalWrite(8, LOW);
-   }else{
-    digitalWrite(8, HIGH);
-   }
-   
-   leftControllerOutput = abs(leftControllerOutput);
-   leftControllerOutput = (leftControllerOutput/8.5)*255;
-   if(leftControllerOutput > 255){
-    leftControllerOutput = 255;
-   }
-   rightControllerOutput = abs(rightControllerOutput);
-   rightControllerOutput = (rightControllerOutput/8.5)*255;
-   if(rightControllerOutput > 255){
-    rightControllerOutput = 255;
-   }
-   analogWrite(9,leftControllerOutput);
-   analogWrite(10,rightControllerOutput);
-   
-   Tc2 = millis();
-   delay(50-(Tc2-Tc));
-   
-   
-   
 //      Serial.print("Tell go a foot ");
 //      Serial.println(distance);
+      delay(100);
       RightWheel();
       LeftWheel();
   }
