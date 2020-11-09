aa572bf21d1efeac44eefdfe45feaaef90b07cdb
mchidalgo <72232681+mchidalgo@users.noreply.github.com>
2020-11-09 15:01:16 -0700

    Update make_markers.py

--- Demo2/make_markers.py
+++ Demo2/make_markers.py
@@ -1,3 +1,20 @@
-cv::Mat markerImage;
-cv::Ptr<cv::aruco::Dictionary> dictionary = cv::aruco::getPredefinedDictionary(cv::aruco::DICT_6X6_250);
-cv::aruco::drawMarker(dictionary, 23, 200, markerImage, 1);
+import numpy as np
+import cv2
+import cv2.aruco as aruco
+
+
+'''
+    drawMarker(...)
+        drawMarker(dictionary, id, sidePixels[, img[, borderBits]]) -> img
+'''
+
+aruco_dict = aruco.Dictionary_get(aruco.DICT_6X6_250)
+print(aruco_dict)
+# second parameter is id number
+# last parameter is total image size
+img = aruco.drawMarker(aruco_dict, 2, 700)
+cv2.imwrite("test_marker.jpg", img)
+
+cv2.imshow('frame',img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
