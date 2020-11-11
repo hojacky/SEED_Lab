1ddff5e16db3e6a45f602f2959e0cfbffe0f386a
mchidalgo <72232681+mchidalgo@users.noreply.github.com>
2020-11-11 15:05:29 -0700

    Update Demo2_pi_video.py

--- Demo2/Demo2_pi_video.py
+++ Demo2/Demo2_pi_video.py
@@ -18,11 +18,11 @@ import busio
 import adafruit_character_lcd.character_lcd_rgb_i2c as character_lcd
 
 def marker_detection():
-	#setup for video and resolution
-	cap = cv2.VideoCapture(0)
-	cap.set(3, 1920)
-	cap.set(4,1080)
-	ret, image = cap.read()
+    #setup for video and resolution
+    cap = cv2.VideoCapture(0)
+    cap.set(3, 1920)
+    cap.set(4,1080)
+    ret, image = cap.read()
               
     #Convert image to grayscale
     image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
@@ -37,46 +37,22 @@ def marker_detection():
     #checks if any aruco markers were detected
     if ids == None:
         print("No markers found.")
-		return False
+        return False
     else:
         return True
 
 def angle_detection():
     fov = 54        #field of view of camera
     image_length = 1920     #length of image in px
-	f = 1550        #focal length of camera in px
-	##########################################
-	##########################################
-	##########################################
-	##########################################
-	##########################################
-	##########################################
-	##########################################
-	##########################################
-	##########################################
-	##########################################
-	##########################################
-	##########################################
-    X_real = 189    #actual length of aruco marker in px CHANGE TO THE ACTUAL LENGTH OF THE MARKER FROM THE DEMO
-	##########################################
-	##########################################
-	##########################################
-	##########################################
-	##########################################
-	##########################################
-	##########################################
-	##########################################
-	##########################################
-	##########################################
-	##########################################
-	##########################################
+    f = 1550        #focal length of camera in px
+    X_real = 697.32    #actual length of aruco marker in px CHANGE TO THE ACTUAL LENGTH OF THE MARKER FROM THE DEMO
     
 
     #setup for video and resolution
-	cap = cv2.VideoCapture(0)
-	cap.set(3, 1920)
-	cap.set(4,1080)
-	ret, image = cap.read()
+    cap = cv2.VideoCapture(0)
+    cap.set(3, 1920)
+    cap.set(4,1080)
+    ret, image = cap.read()
               
     #Convert image to grayscale
     image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
@@ -91,11 +67,11 @@ def angle_detection():
     #checks if any aruco markers were detected
     if ids == None:
         print("No markers found.")
-		return False, 0
-    else:		
+        return False, 0
+    else:       
         for i in range(len(ids)):
             c = corners[i][0]
-		
+        
             #Angle calculation
             x_center = (c[0][0] + c[1][0] + c[2][0] + c[3][0])/4
             deg = (x_center / image_length)*(fov)
@@ -109,13 +85,16 @@ def angle_detection():
                 deg = deg - 27
                 print(deg)
                 return True, deg
-			
+            
 def dist_detection():
-	#setup for video and resolution
-	cap = cv2.VideoCapture(0)
-	cap.set(3, 1920)
-	cap.set(4,1080)
-	ret, image = cap.read()
+    image_length = 1920     #length of image in px
+    f = 1550        #focal length of camera in px
+    X_real = 697.32    #actual length of aruco marker in px CHANGE TO THE ACTUAL LENGTH OF THE MARKER 
+    #setup for video and resolution
+    cap = cv2.VideoCapture(0)
+    cap.set(3, 1920)
+    cap.set(4,1080)
+    ret, image = cap.read()
               
     #Convert image to grayscale
     image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
@@ -130,26 +109,27 @@ def dist_detection():
     #checks if any aruco markers were detected
     if ids == None:
         print("No markers found.")
-		return False
+        return False
     else:
         for i in range(len(ids)):
             c = corners[i][0]
-					
-			#Distance calculation
-		    #gather position of corners and gets height
-			y1 = c[2][1] - c[0][1]
-			y2 = c[1][1] - c[3][1]
-	   		#gathers average of both heights
-			y = (y1 + y2)/2
-			if y < 0:
-			    y = y * -1
-			#solve for distance using perspective projection equation
-			#DON'T FORGET TO CHANGE X_real
-			Z = f*X_real/y
-			#convert pixels to mm
-			Z = Z / 3.77953
-			print("The distance of id %d is %d mm" %(ids[i], Z))
-			return True, Z
+                    
+            #Distance calculation
+            #gather position of corners and gets height
+            y1 = c[2][1] - c[0][1]
+            y2 = c[1][1] - c[3][1]
+            #gathers average of both heights
+            y = (y1 + y2)/2
+            if y < 0:
+                y = y * -1
+            #solve for distance using perspective projection equation
+            #DON'T FORGET TO CHANGE X_real
+            Z = f*X_real/y
+            #convert pixels to mm then feet
+            Z = Z / 3.77953
+            Z = Z/305
+            print("The distance of id %d is %d ft" %(ids[i], Z))
+            return True, Z
 
 #Set up for sending values to Arduino
 bus = smbus.SMBus(1)
@@ -168,35 +148,41 @@ def readNumber():
     return number
         
 if __name__ == '__main__':
-	marker_found = False
-	angle_found = False
-	dist_found = False
-	
-	#continue video capture until a marker is found
-	while (marker_found == false):
-		marker_found = marker_detection
-	writeNumber(0)		#write to arduino to tell it to stop motors
-	while (readNumber != 1):		#wait until the motor stops and arduino sends a number to tell the pi to start looking for angle
-		pass
-	print("Start detecting for angle")
-	
-	#detect the angle of the aruco marker
-	while (angle_found == False):
-		angle_found, angle = angle_detection()
-	writeNumber(angle)		#write angle to arduino
-	#wait until the robot is centered onto the marker
-	while (readNumber != 1):
-		pass
-	print("Start detecting for distance")
-	
-	#detect the distance of the aruco marker
-	while (dist_found == False):
-		dist_found, distance = dist_detection()
-	writeNumber(distance)
-	
-		
-	
+    marker_found = False
+    angle_found = False
+    dist_found = False
+    
+    #continue video capture until a marker is found
+    while (marker_found == False):
+        marker_found = marker_detection
+    writeNumber(0)      #write to arduino to tell it to stop motors
+    while (readNumber != 1):        #wait until the motor stops and arduino sends a number to tell the pi to start looking for angle
+        pass
+    print("Start detecting for angle")
+    
+    #detect the angle of the aruco marker
+    while (angle_found == False):
+        angle_found, angle = angle_detection()
+    
+    writeNumber(2)
+    sleep(2)
+    writeNumber(angle)      #write angle to arduino
+    #wait until the robot is centered onto the marker
+    while (readNumber != 1):
+        pass
+    print("Start detecting for distance")
+    
+    #detect the distance of the aruco marker
+    while (dist_found == False):
+        dist_found, distance = dist_detection()
+    writeNumber(3)
+    sleep(2)
+    writeNumber(distance)
+    
+        
+    
 
 
-	
+    
             
+
